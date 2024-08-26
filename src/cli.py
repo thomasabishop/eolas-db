e7518a3654d8ecb59f7ada83912801426316598c
thomasabishop <tactonbishop@gmail.com>
2024-08-26 16:21:52 +0100

    feat: setup basic CLI structure

--- src/cli.py
+++ src/cli.py
@@ -1,5 +1,18 @@
+import argparse
+
+
 def main():
-    print("Application is running")
+    parser = argparse.ArgumentParser(
+        prog="eolas-db", description="Eolas database manager."
+    )
+    parser.add_argument("command", choices=["parse"], help="Command to execute")
+    parser.add_argument("--path", help="Path to Zettelkasten directory")
+    args = parser.parse_args()
+
+    print("Welcome to eolas-db")
+
+    if args.command == "parse":
+        pass
 
 
 if __name__ == "__main__":
