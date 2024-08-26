51e35a8fee84accd6c2e1bd62c5b41ae39326eba
thomasabishop <tactonbishop@gmail.com>
2024-08-26 16:01:15 +0100

    feat: set-up entry point and install

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="eolas-db",
+    version="0.1",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        # List your project dependencies here
+    ],
+    entry_points={
+        "console_scripts": [
+            "eolas-db=cli:main",
+        ],
+    },
+)
---
+++ src/cli.py
@@ -0,0 +1,6 @@
+def main():
+    print("Application is running")
+
+
+if __name__ == "__main__":
+    main()
