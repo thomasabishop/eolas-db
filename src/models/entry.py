725adf65ccddbd1b9c2982df4ced5a1b38382837
thomasabishop <tactonbishop@gmail.com>
2024-11-11 14:47:59 +0000

    feature: return raw bytes as file size

--- src/models/entry.py
+++ src/models/entry.py
@@ -4,7 +4,7 @@ from typing import List, TypedDict
 class Entry(TypedDict):
     title: str
     last_modified: str
-    size: str
+    size: int
     tags: List[str]
     links: List[str]
     body: str
